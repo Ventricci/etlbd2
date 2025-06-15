8f1f91bf6e7672c2e9db4b266816daf03ac5cae4
Alexsander Ventricci <alexsander.ventricci@gmail.com>
2025-06-15 19:08:37 -0300

    add readme

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ventricci/Documentos/BD2/venv
+set -gx VIRTUAL_ENV /home/ventricci/Documentos/BD2/Migration/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
